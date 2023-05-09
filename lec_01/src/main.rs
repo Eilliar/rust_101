0b5d5c46b0c2c270cd6394be69226a71ba7ecb28
Eilliar <engbrunoeilliar@gmail.com>
2023-05-08 22:43:06 -0300

    variables

--- lec_01/src/main.rs
+++ lec_01/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
+    let mut x = 10; // if mut not present, cannot re-assign it to 20
+    let name = "General Kenobi";
+
     // println! is a macro, not a function
-    println!("Hello, world!");
+    println!("Hello, there {}!", name);
+    println!("x: {}", x);
+    x = 20;
+    println!("x: {}", x);
 }
