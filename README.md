6e5c99e9c504849b8e1279dfd87b4c5f2b2f1747
Eilliar <engbrunoeilliar@gmail.com>
2023-05-08 22:47:29 -0300

    Update doc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rust_101
+
 Rust development courses
+
+
+## References
+
+- [Rust naming conventions](https://rust-lang.github.io/api-guidelines/naming.html)
--- lec_01/src/main.rs
+++ lec_01/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let mut x = 10; // if mut not present, cannot re-assign it to 20
+    // Variables are imutable by default
+    // if mut not present, cannot re-assign it to 20
+    let mut x = 10; 
     let name = "General Kenobi";
 
     // println! is a macro, not a function
