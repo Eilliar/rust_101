853ecce9dfdc4032e68fedc11ad7e5fc7a22a2a1
Eilliar <engbrunoeilliar@gmail.com>
2023-05-08 23:01:24 -0300

    add float variable

--- lec_01/src/main.rs
+++ lec_01/src/main.rs
@@ -1,12 +1,16 @@
 fn main() {
     // Variables are imutable by default
     // if mut not present, cannot re-assign it to 20
-    let mut x = 10; 
+    let mut x: u8 = 254; 
+    let y: f64 = 0.5456834013658346;
     let name = "General Kenobi";
 
     // println! is a macro, not a function
     println!("Hello, there {}!", name);
+    
     println!("x: {}", x);
-    x = 20;
+    x += 1;
     println!("x: {}", x);
+
+    println!("y: {}", y);
 }
